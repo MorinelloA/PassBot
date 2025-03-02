905eba560ae51e4bb84ef23e3e57f68f17e2737a
Tony Morinello <morinelloa@gmail.com>
2025-03-01 19:19:23 -0500

    Fixed email validation

--- PassBot/Utilities/ValidationUtils.cs
+++ PassBot/Utilities/ValidationUtils.cs
@@ -6,15 +6,14 @@ namespace PassBot.Utilities
     {
         public static bool IsValidEmail(string email)
         {
-            try
-            {
-                var emailRegex = new System.Text.RegularExpressions.Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
-                return emailRegex.IsMatch(email);
-            }
-            catch
-            {
-                return false;
-            }
+            const string pattern = @"^(?!.*[<>])(?:(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*)|(?:""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*""))@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+
+            var emailRegex = new System.Text.RegularExpressions.Regex(
+                pattern,
+                RegexOptions.IgnoreCase
+            );
+
+            return emailRegex.IsMatch(email);
         }
 
         public static bool IsValidEthereumAddress(string walletAddress)
